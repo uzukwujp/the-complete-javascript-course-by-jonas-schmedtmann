18ca773b7c1396e50b6d0fb0065df929ee779661
Andre MacNamara <andremacnamara12@gmail.com>
2019-04-07 19:38:16 +0100

    Data Types and Variables

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,18 @@
+/*
+* Data Types and Varaibles
+*/
+
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 20; 
+var fullAge = true;
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job)
+
+
