cfe062dcb7bee6ae71a32d0e0dba6728d35c2470
Andre MacNamara <andremacnamara12@gmail.com>
2019-05-06 13:00:37 +0100

    Boolean logic

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -131,26 +131,39 @@
 * IF/ELSE 
 */
 
+// var firstName = 'John';
+// var civilStatus = 'single';
+
+// if (civilStatus === 'married') {
+//     console.log(firstName + ' is married');
+// } else {
+//     console.log(firstName + ' will hopefully marry soon');
+// }
+
+// var isMarried = true;
+
+// if(isMarried){
+//     console.log(firstName + ' is married');
+// } else {
+//     console.log(firstName + ' will hopefully marry soon');
+// }
+
+/**************************
+* Boolean Logic
+*/
+
 var firstName = 'John';
-var civilStatus = 'single';
+var age = 20;
 
-if (civilStatus === 'married') {
-    console.log(firstName + ' is married');
+if (age < 13) {
+    console.log(firstName + ' is a boy');
+} else if (age >= 13 && age < 20) {
+    console.log(firstName + ' is a teenager.');
+} else if (age >= 20 && age < 30) {
+    console.log(firstName + ' is a young man.');
 } else {
-    console.log(firstName + ' will hopefully marry soon');
+    console.log(firstName + ' is a man');
 }
-
-var isMarried = true;
-
-if(isMarried){
-    console.log(firstName + ' is married');
-} else {
-    console.log(firstName + ' will hopefully marry soon');
-}
-
-
-
-
 
 
 
