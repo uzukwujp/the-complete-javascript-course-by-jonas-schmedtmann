05eef8aab1432e6a6a159cf4a5c27b8e18d13c4c
Andre MacNamara <andremacnamara12@gmail.com>
2019-05-08 16:34:40 +0100

    Game init function

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -11,19 +11,9 @@ GAME RULES:
 
 var scores, roundScore, activePlayer;
 
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
+init();
 
-document.querySelector('.dice').style.display = 'none';
-
-//Initialize all scores to 0
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
-
-//Generate the dice roll.
+/* --- Generate the dice roll. --- */
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
     // 1. Generate random number between 1 - 6;
@@ -44,8 +34,9 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     }
 });
 
-    // Button Hold
-    document.querySelector('.btn-hold').addEventListener('click', function() {
+/* --- Button Hold --- */
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
     // Add CURRENT score to GLOBAL score
     scores[activePlayer] += roundScore;
 
@@ -62,10 +53,11 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     } else {
         nextPlayer();
     }
-
-   
-
 });
+
+/* --- Reset the Game --- */
+
+document.querySelector('.btn-new').addEventListener('click', init);
 
 function nextPlayer() {
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
@@ -78,4 +70,25 @@ function nextPlayer() {
     document.querySelector('.player-1-panel').classList.toggle('active');
 
     document.querySelector('.dice').style.display = 'none';
+}
+
+function init() {
+    scores = [0,0];
+    roundScore = 0;
+    activePlayer = 0;
+
+    document.querySelector('.dice').style.display = 'none';
+
+    //Initialize all scores to 0
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
 }
