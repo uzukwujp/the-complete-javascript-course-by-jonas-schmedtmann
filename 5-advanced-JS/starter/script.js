5d0056d499dfc6103c3bac6a6e59547125cf4f48
Andre MacNamara <andremacnamara12@gmail.com>
2019-05-09 13:40:13 +0100

    Object.create

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -1,5 +1,6 @@
 // Function Constructor
 
+/*
 var Person = function(name, yearOfBirth, job) 
 {
     this.name = name;
@@ -13,17 +14,38 @@ Person.prototype.calculateAge = function(){
 
 Person.prototype.lastName = 'Smith';
 
-/*
+
     1. New empty object is created.
     2. The function is called.
     3. 'this' points to the new empty object, not the global variables.
 */
-
+/*
 var andre = new Person('Andre', 1996, 'Software Engineer');
 var cathal = new Person('Cathal', 1997, 'Teacher');
+
 
 andre.calculateAge();
 cathal.calculateAge();
 
 console.log(andre);
-console.log(andre.lastName);
+console.log(andre.lastName)*/
+
+// Object.Create
+
+var personProto = {
+    calculateAge: function() {
+        console.log(2016 - this.yearOfBirth);
+    }
+};
+
+var john = Object.create(personProto);
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'teacher';
+
+var jane = Object.create(personProto, {
+    name: {value: 'Jane'},
+    yearOfBirth: {value: 1990},
+    job: {value: 'designer'}
+});
+
