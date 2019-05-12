185190e338f45a8cfea6efbb3a021b36fc0fd64f
Andre MacNamara <andremacnamara12@gmail.com>
2019-05-12 21:57:48 +0100

    Part 1 - My Solution

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -273,62 +273,145 @@ console.log(andre.lastName)*/
 
 /*--- Bind, Call * Apply --- */
 
-var john = {
-    name: 'John',
-    age: 26,
-    job: 'teacher',
-    presentation: function(style, timeOfDay) {
-        if (style === 'formal') {
-            console.log('Good ' + timeOfDay + ', Ladies and gentlemen! I\'m ' +  this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old.');
-        } else if (style === 'friendly') {
-            console.log('Hey! What\'s up? I\'m ' +  this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old. Have a nice ' + timeOfDay + '.');
-        }
-    }
-};
-var emily = {
-    name: 'Emily',
-    age: 35,
-    job: 'designer'
-};
+// var john = {
+//     name: 'John',
+//     age: 26,
+//     job: 'teacher',
+//     presentation: function(style, timeOfDay) {
+//         if (style === 'formal') {
+//             console.log('Good ' + timeOfDay + ', Ladies and gentlemen! I\'m ' +  this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old.');
+//         } else if (style === 'friendly') {
+//             console.log('Hey! What\'s up? I\'m ' +  this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old. Have a nice ' + timeOfDay + '.');
+//         }
+//     }
+// };
+// var emily = {
+//     name: 'Emily',
+//     age: 35,
+//     job: 'designer'
+// };
 
-john.presentation('formal', 'morning');
-john.presentation.call(emily, 'formal', 'morning');
+// john.presentation('formal', 'morning');
+// john.presentation.call(emily, 'formal', 'morning');
 
 
 
-var john = {
-    name: 'John',
-	salary: 30000,
-  	bonus: 5000,
-  	total: function(){
-    	console.log(this.name + ' made ' + (this.salary + this.bonus));
-    }
-}
+// var john = {
+//     name: 'John',
+// 	salary: 30000,
+//   	bonus: 5000,
+//   	total: function(){
+//     	console.log(this.name + ' made ' + (this.salary + this.bonus));
+//     }
+// }
 
-var emily = {
-    name: 'Emily',
-	salary: 30000,
-  	bonus: 5500,
-}
+// var emily = {
+//     name: 'Emily',
+// 	salary: 30000,
+//   	bonus: 5500,
+// }
 
-john.total(); //John made 35000
-john.total.call(emily); //Emily made 35500
+// john.total(); //John made 35000
+// john.total.call(emily); //Emily made 35500
 
-var person = {
-    fullName: function() {
-        console.log(this.firstName + " " + this.lastName);
-    }
-  }
-  var person1 = {
-    firstName: "Jack",
-    lastName: "Smith"
-  }
+// var person = {
+//     fullName: function() {
+//         console.log(this.firstName + " " + this.lastName);
+//     }
+//   }
+//   var person1 = {
+//     firstName: "Jack",
+//     lastName: "Smith"
+//   }
   
-person.fullName.apply(person1);  // Will return "Jack Smith"
+// person.fullName.apply(person1);  // Will return "Jack Smith"
 
-//bind
-var johnFriendly = john.presentation.bind(john, 'friendly');
-johnFriendly('morning');
-johnFriendly('night');
-var emilyFormal = john.presentation.bind(emily, 'formal');
-emilyFormal('afternoon');
+// //bind
+// var johnFriendly = john.presentation.bind(john, 'friendly');
+// johnFriendly('morning');
+// johnFriendly('night');
+// var emilyFormal = john.presentation.bind(emily, 'formal');
+// emilyFormal('afternoon');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////////
+// CODING CHALLENGE
+
+
+/*
+--- Let's build a fun quiz game in the console! ---
+1. Build a function constructor called Question to describe a question. A question should include:
+a) question itself
+b) the answers from which the player can choose the correct one (choose an adequate data structure here, array, object, etc.)
+c) correct answer (I would use a number for this)
+2. Create a couple of questions using the constructor
+3. Store them all inside an array
+4. Select one random question and log it on the console, together with the possible answers (each question should have a number) (Hint: write a method for the Question objects for this task).
+5. Use the 'prompt' function to ask the user for the correct answer. The user should input the number of the correct answer such as you displayed it on Task 4.
+6. Check if the answer is correct and print to the console whether the answer is correct ot nor (Hint: write another method for this).
+7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
+*/
+
+/*
+--- Expert level ---
+8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
+9. Be careful: after Task 8, the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
+10. Track the user's score to make the game more fun! So each time an answer is correct, add 1 point to the score (Hint: I'm going to use the power of closures for this, but you don't have to, just do this with the tools you feel more comfortable at this point).
+11. Display the score in the console. Use yet another method for this.
+*/
+
+
+let questionAsked;
+
+//Create Questions
+function Question(question, answers, correctAnswer){
+    this.question = question;
+    this.answers = answers;
+    this.correctAnswer = correctAnswer;
+}
+
+var question1 = new Question('Where are Liverpool in the league?', ['1st', '2nd', '3rd'], 1);
+var question2 = new Question('Which player didn\'t win the golden boot?', ['Salah', 'Mane', 'Aguero'], 2);
+var question3 = new Question('Which player won the golden glove?', ['Allison ', 'Ederson ', 'De Gea 🤣'], 0);
+
+var questionsArr = [];
+questionsArr.push(question1, question2, question3);
+
+//Select Random question
+function selectRandomQuestion(){
+    selectedQuestionNumber = Math.floor(Math.random() * questionsArr.length);
+    var selectedQuestion = questionsArr[selectedQuestionNumber];
+    questionAsked = selectedQuestion;
+    console.log('Question: ' + selectedQuestion['question']);
+    console.log('Possible Answers: ' + selectedQuestion['answers']);
+}
+
+selectRandomQuestion();
+
+//Prompt for answer
+var userAnswer = prompt('Enter the number of the correct answer (0 - 2)');
+function checkAnswer(){
+    if(userAnswer == questionAsked['correctAnswer']){
+        console.log('Correct! ' + questionAsked['answers'][questionAsked['correctAnswer']]);
+    } else {
+        console.log('Wrong');
+    }
+}
+
+checkAnswer();
