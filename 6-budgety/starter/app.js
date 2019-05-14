bfb43e2dda189e1833edd5c59d1ab7aba725fcb8
Andre MacNamara <andremacnamara12@gmail.com>
2019-05-14 14:15:02 +0100

    Calculating budget values.

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -22,6 +22,18 @@ var budgetController = (function() {
         this.value = value;
     };
 
+    var calculateTotal = function(type) {
+        // Add each value in the array to the current sum value.
+
+        var sum = 0;
+
+        data.allItems[type].forEach(function(current) {
+            sum = sum + current.value;
+        });
+
+        data.totals[type] = sum;
+    };
+
     var data = {
        allItems: {
            exp: [],
@@ -31,7 +43,10 @@ var budgetController = (function() {
        totals: {
            exp: 0,
            inc: 0
-       }
+       },
+       budget: 0,
+       percentage: -1
+
     }
 
     return {
@@ -57,6 +72,32 @@ var budgetController = (function() {
 
             // Return the new element;.
             return newItem;
+        },
+
+        calculateBudget: function () {
+
+            // Calculate total income and expenses
+            calculateTotal('exp');
+            calculateTotal('inc');
+
+            // Calculate the budget: income - expenses
+            data.budget = data.totals.inc - data.totals.exp;
+
+            // Calculate the percentage of income that we spent
+            if (data.totals.inc > 0) {
+                data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+            } else {
+                data.percentage = -1;
+            }
+        },
+
+        getBudget: function() {
+            return {
+                budget: data.budget,
+                totalInc: data.totals.inc,
+                totalExp: data.totals.exp,
+                percentage: data.percentage
+            }
         },
 
         testing: function() {
@@ -133,8 +174,6 @@ var UIController = (function() {
             // Insert the HTML into the DOM
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         }
-
-
     }
 })();
 
@@ -157,10 +196,13 @@ var controller = (function(budgetCtrl, UICtrl) {
     
     var updateBudget = function() {
         // 1. Calculate the budget
-        
+        budgetCtrl.calculateBudget();
+
         // 2. Return the budget
+        var budget = budgetCtrl.getBudget();
 
         // 3. Display the budget on the UI 
+        console.log(budget);
     }
 
     var ctrlAddItem = function() {
