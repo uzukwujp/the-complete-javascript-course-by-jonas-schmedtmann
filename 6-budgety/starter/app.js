f4b1133d94613b55a50bab542c8877073c40c0cd
Andre MacNamara <andremacnamara12@gmail.com>
2019-05-14 13:36:12 +0100

    Check for valid input

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -86,7 +86,7 @@ var UIController = (function() {
             return {
                 type: document.querySelector(DomStrings.inputType).value,
                 description: document.querySelector(DomStrings.inputDescription).value,
-                value: document.querySelector(DomStrings.inputValue).value
+                value: parseFloat(document.querySelector(DomStrings.inputValue).value)
             }
         },
         
@@ -155,6 +155,14 @@ var controller = (function(budgetCtrl, UICtrl) {
         });
     };
     
+    var updateBudget = function() {
+        // 1. Calculate the budget
+        
+        // 2. Return the budget
+
+        // 3. Display the budget on the UI 
+    }
+
     var ctrlAddItem = function() {
 
         var input, newItem;
@@ -162,19 +170,19 @@ var controller = (function(budgetCtrl, UICtrl) {
         // 1. Get the input data
         input = UICtrl.getInput();
 
-        // 2. Add item to the budget controller
-        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+        if (input.description !== "" && !isNaN(input.value) && input.value > 0) {
+            // 2. Add item to the budget controller
+            newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
-        // 3. Add the item to the UI
-        UICtrl.addListItem(newItem, input.type);
+            // 3. Add the item to the UI
+            UICtrl.addListItem(newItem, input.type);
 
-        //4. Clear the fields
-        UICtrl.clearFields();
-        
-        // 5. Calculate the budger
+            //4. Clear the fields
+            UICtrl.clearFields();
 
-        // 6. Display the budget on the UI 
-        console.log('It works');
+            // 5. Calculate and update budget
+            updateBudget();
+        }
     }
 
     return {
