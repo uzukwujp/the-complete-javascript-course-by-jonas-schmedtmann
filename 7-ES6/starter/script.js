e8df2f39bb613f4b1d6de4c6fa1b56e22b11d4f0
Andre MacNamara <andremacnamara12@gmail.com>
2019-05-18 12:48:27 +0100

    Blocks and IIFE's

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -1,52 +1,78 @@
 // Lecture: let and const
 
 
-// ES5
-var name5 = 'Jane Smith';
-var age5 = 23;
-name5 = 'Jane Miller';
-console.log(name5);
+// // ES5
+// var name5 = 'Jane Smith';
+// var age5 = 23;
+// name5 = 'Jane Miller';
+// console.log(name5);
+
+// // ES6
+// const name6 = 'Jane Smith';
+// let age = 23;
+// name6 = 'Jane Miller'; // This results in a type error. You can't reassign a constant.
+// console.log(name6);
+
+// // ES5
+// function driversLicence5(passedTest) {
+    
+//     if (passedTest) {
+//         console.log(firstName);
+//         var firstName = 'John';
+//         var yearOfBirth = 1990;
+//     }
+    
+    
+//     console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a car.');
+// }
+
+// driversLicence5(true);
+
+// // ES6
+// function driversLicence6(passedTest) {
+    
+//     //console.log(firstName);
+//     let firstName;
+//     const yearOfBirth = 1990;
+    
+//     if (passedTest) {
+//         firstName = 'John';
+//     }
+    
+//     console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a car.');
+// }
+
+// driversLicence6(true);
+
+// var i = 23;
+
+// for (var i = 0; i < 5; i++) {
+//     console.log(i);
+// }
+// console.log(i);
+
+
+
+
+
+
+// Lecture: Blocks and IIFEs
 
 // ES6
-const name6 = 'Jane Smith';
-let age = 23;
-name6 = 'Jane Miller'; // This results in a type error. You can't reassign a constant.
-console.log(name6);
 
-// ES5
-function driversLicence5(passedTest) {
-    
-    if (passedTest) {
-        console.log(firstName);
-        var firstName = 'John';
-        var yearOfBirth = 1990;
-    }
-    
-    
-    console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a car.');
+{
+    const a = 1;
+    let b = 2;
 }
 
-driversLicence5(true);
+console.log(a + b); //Fails. Variables can't be acessed out of the block.
 
-// ES6
-function driversLicence6(passedTest) {
-    
-    //console.log(firstName);
-    let firstName;
-    const yearOfBirth = 1990;
-    
-    if (passedTest) {
-        firstName = 'John';
-    }
-    
-    console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a car.');
-}
+//ES5
 
-driversLicence6(true);
+(function() {
+    var c = 3;
+})();
 
-var i = 23;
+console.log(c);
 
-for (var i = 0; i < 5; i++) {
-    console.log(i);
-}
-console.log(i);
+
