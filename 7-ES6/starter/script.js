9dacd2d5914a8d564cf10d3ee34cad271bede4dc
Andre MacNamara <andremacnamara12@gmail.com>
2019-05-19 16:10:43 +0100

    Classes

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -472,38 +472,79 @@ isFullAge6(1990, 1999, 1965);
 //A new data strusture data take a key/value pair.
 
 
-const question = new Map();
-question.set('question', 'What is the offical name of the latest major JavaScrip version?');
-question.set(1, 'ES5');
-question.set(2, 'ES6')
-question.set(3, 'ES2015')
-question.set(4, 'ES7')
-question.set('correct', 3);
-question.set(true, 'Correct answer');
-question.set(false, 'Wrong Answer');
+// const question = new Map();
+// question.set('question', 'What is the offical name of the latest major JavaScrip version?');
+// question.set(1, 'ES5');
+// question.set(2, 'ES6')
+// question.set(3, 'ES2015')
+// question.set(4, 'ES7')
+// question.set('correct', 3);
+// question.set(true, 'Correct answer');
+// question.set(false, 'Wrong Answer');
 
-// Get the data
-console.log(question.get('question'));
-console.log(question.size);
+// // Get the data
+// console.log(question.get('question'));
+// console.log(question.size);
 
-// if(question.has(4)) {
-//     question.delete(4);
+// // if(question.has(4)) {
+// //     question.delete(4);
+// // }
+
+// // question.clear();
+
+
+// // Iterate
+
+// // question.forEach((value, key) => 
+// // console.log(`This is ${key}, and it's set to ${value}`));
+
+// for (let [key, value] of question.entries()) {
+//     if (typeof(key) === 'number') {
+//         console.log(`Answer ${key}: ${value}`);
+//     }
 // }
 
-// question.clear();
+// const ans = parseInt(prompt('Write the correct answer'));
+
+// console.log(question.get(ans === question.get('correct')));
 
 
-// Iterate
+// Lecture: Classes
 
-// question.forEach((value, key) => 
-// console.log(`This is ${key}, and it's set to ${value}`));
+//ES5
 
-for (let [key, value] of question.entries()) {
-    if (typeof(key) === 'number') {
-        console.log(`Answer ${key}: ${value}`);
+var Person5 = function(name, yearOfBirth, job) 
+{
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+}
+
+Person5.prototype.calculateAge = function() {
+    var age = new Date().getFullYear - this.yearOfBirth;
+    console.log(age);
+}
+
+var john5 = new Person5('John', 1990, 'teacher');
+
+// ES6
+class Person6 {
+    constructor (name, yearOfBirth, job) {
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+        this.job = job;
+    }
+
+    calculateAge() {
+        var age = new Date().getFullYear - this.yearOfBirth;
+        console.log(age);
+    }
+
+    static greeting() {
+        console.log('Hey there');
     }
 }
 
-const ans = parseInt(prompt('Write the correct answer'));
-
-console.log(question.get(ans === question.get('correct')));
+const john6 = new Person6('John', 1990, 'teacher');
+Person6.greeting();
+console.log(john6);
