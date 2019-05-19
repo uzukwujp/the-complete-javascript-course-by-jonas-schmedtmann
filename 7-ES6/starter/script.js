7ec9c8627a4ce6fef773b0eb6c95ab36ed934258
Andre MacNamara <andremacnamara12@gmail.com>
2019-05-19 15:52:23 +0100

    Maps

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -454,12 +454,56 @@ isFullAge6(1990, 1999, 1965);
 
 //ES6
 
-function SmithPerson(firstName, yearOfBirth, lastName = 'Smith', nationality = 'American') {
-    this.firstName = firstName;
-    this.yearOfBirth = yearOfBirth;
-    this.lastName = lastName;
-    this.nationality = nationality;
+// function SmithPerson(firstName, yearOfBirth, lastName = 'Smith', nationality = 'American') {
+//     this.firstName = firstName;
+//     this.yearOfBirth = yearOfBirth;
+//     this.lastName = lastName;
+//     this.nationality = nationality;
+// }
+
+// var john = new SmithPerson('John', 1991);
+// console.log(john);
+
+
+
+
+//// Maps
+
+//A new data strusture data take a key/value pair.
+
+
+const question = new Map();
+question.set('question', 'What is the offical name of the latest major JavaScrip version?');
+question.set(1, 'ES5');
+question.set(2, 'ES6')
+question.set(3, 'ES2015')
+question.set(4, 'ES7')
+question.set('correct', 3);
+question.set(true, 'Correct answer');
+question.set(false, 'Wrong Answer');
+
+// Get the data
+console.log(question.get('question'));
+console.log(question.size);
+
+// if(question.has(4)) {
+//     question.delete(4);
+// }
+
+// question.clear();
+
+
+// Iterate
+
+// question.forEach((value, key) => 
+// console.log(`This is ${key}, and it's set to ${value}`));
+
+for (let [key, value] of question.entries()) {
+    if (typeof(key) === 'number') {
+        console.log(`Answer ${key}: ${value}`);
+    }
 }
 
-var john = new SmithPerson('John', 1991);
-console.log(john);
+const ans = parseInt(prompt('Write the correct answer'));
+
+console.log(question.get(ans === question.get('correct')));
