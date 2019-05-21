5f2846ffd81e689e65463badd1ee041e7b3a4505
Andre MacNamara <andremacnamara12@gmail.com>
2019-05-21 20:24:47 +0100

    Search model

--- 9-forkify/src/js/index.js
+++ 9-forkify/src/js/index.js
@@ -1,20 +1,5 @@
+import Search from './models/Search';
 
-// https://www.food2fork.com/api/search
-// a3c57d026d4193f715cafef824dd1faf
-
-import axios from 'axios';
-
-async function getResults(query) 
-{
-    const key = 'a3c57d026d4193f715cafef824dd1faf';
-
-    try {
-        const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
-        const recipes = res.data.recipes;
-        console.log(recipes);
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-getResults('chinese');
+const search = new Search('pizza');
+// console.log(search);
+search.getResults();
--- 9-forkify/src/js/models/Search.js
+++ 9-forkify/src/js/models/Search.js
@@ -1,1 +1,21 @@
-export default 'I am an exported string';
+import axios from 'axios';
+
+export default class Search {
+    constructor(query)
+    {
+        this.query = query;
+    }
+
+    async getResults() 
+    {
+        const key = 'a3c57d026d4193f715cafef824dd1faf';
+
+        try {
+            const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+            this.result = res.data.recipes;
+            // console.log(this.result);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
