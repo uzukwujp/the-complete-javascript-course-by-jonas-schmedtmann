a9d88e2ae8254e76af08b3db604ce7b293bb1e93
Andre MacNamara <andremacnamara12@gmail.com>
2019-05-21 20:47:30 +0100

    Search Controller

--- 9-forkify/src/js/index.js
+++ 9-forkify/src/js/index.js
@@ -1,5 +1,39 @@
 import Search from './models/Search';
 
-const search = new Search('pizza');
+/** Global state of the app
+ * Search Object
+ * Current recipe object
+ * Shopping list object
+ * Liked recipes
+ */
+
+const state = [];
+
+const controlSearch = async () => {
+    // 1. Get the query from the view
+    const query = 'pizza';
+
+    if (query) 
+    {
+        // 2. New search object and add to state.
+        state.search = new Search(query);
+
+        // 3. Prepate UI for results
+
+        // 4. Search for recipes
+        await state.search.getResults();
+
+        // Render result on UI
+        console.log(state.search.result);
+    }
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
+// const search = new Search('pizza');
 // console.log(search);
-search.getResults();
+// search.getResults();
+
--- 9-forkify/src/js/models/Search.js
+++ 9-forkify/src/js/models/Search.js
@@ -9,9 +9,10 @@ export default class Search {
     async getResults() 
     {
         const key = 'a3c57d026d4193f715cafef824dd1faf';
+        const key2 = 'f55920ca3ce962617dc9df23d3e0e252';
 
         try {
-            const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+            const res = await axios(`https://www.food2fork.com/api/search?key=${key2}&q=${this.query}`);
             this.result = res.data.recipes;
             // console.log(this.result);
         } catch (error) {
