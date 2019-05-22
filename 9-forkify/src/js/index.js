deb0324535a34cd6bba31596d34b3baa46e357e0
Andre MacNamara <andremacnamara12@gmail.com>
2019-05-22 13:35:18 +0100

    Rendered API results on UI

--- 9-forkify/src/js/index.js
+++ 9-forkify/src/js/index.js
@@ -1,4 +1,6 @@
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
 
 /** Global state of the app
  * Search Object
@@ -11,7 +13,7 @@ const state = [];
 
 const controlSearch = async () => {
     // 1. Get the query from the view
-    const query = 'pizza';
+    const query = searchView.getInput();
 
     if (query) 
     {
@@ -19,21 +21,19 @@ const controlSearch = async () => {
         state.search = new Search(query);
 
         // 3. Prepate UI for results
-
+        searchView.clearInput();
+        searchView.clearResults();
         // 4. Search for recipes
         await state.search.getResults();
 
         // Render result on UI
-        console.log(state.search.result);
+        searchView.renderResults(state.search.result);
     }
 }
 
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
 });
 
-// const search = new Search('pizza');
-// console.log(search);
-// search.getResults();
 
